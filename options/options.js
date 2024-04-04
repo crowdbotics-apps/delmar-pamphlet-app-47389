07e736d6f576542c8f244f5a7559ee8f3f72db8e
Crowdbotics <team@crowdbotics.com>
2024-04-04 18:39:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delmar-pamphlet-app-47389",
+  url: "https://delmar-pamphlet-app-47389.botics.co",
+  api: "https://delmar-pamphlet-app-47389.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
